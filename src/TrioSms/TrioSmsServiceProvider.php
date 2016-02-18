37bed8a80ae9fc9319518994d4901036684dd0c8
Noorzaini Ilhami <ilhami@terato.com>
2016-02-18 12:30:16 +0800

    Fixed typo

--- src/TrioSms/TrioSmsServiceProvider.php
+++ src/TrioSms/TrioSmsServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class TrioSmsServiceProvide extends ServiceProvider
+class TrioSmsServiceProvider extends ServiceProvider
 {
 
     public function boot()
